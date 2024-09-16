8f26fa94c5fbb8768569b434fd7ca14b322a7567
Hayk Ziroyan <haik.ziroan@gmail.com>
2024-09-16 19:02:54 +0400

    last changes for dropdown

--- 1. test-team-library/main.js
+++ 1. test-team-library/main.js
@@ -3,16 +3,4 @@ function addSubCategories() {
 }
 function addCategories() {
     document.getElementById("addCategory").classList.toggle("show");
-  }
-  //   window.onclick = function(event) {
-//     if (!event.target.matches('.dropbtn')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
+}
