67b1977549ce8cc8f2fa2a9b9fc4332e21ed7add
Hayk Ziroyan <haik.ziroan@gmail.com>
2024-09-16 21:49:25 +0400

    addProduct change

--- 1. test-team-library/main.js
+++ 1. test-team-library/main.js
@@ -4,3 +4,17 @@ function addSubCategories() {
 function addCategories() {
     document.getElementById("addCategory").classList.toggle("show");
 }
+fetch('http://localhost:3000/users')
+  .then(response => response.json())
+  .then(users => console.log(galery));
+  const newUser = { name: 'New User', email: 'newuser@example.com' };
+
+fetch('http://localhost:3000/galery', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(newUser),
+})
+.then(response => response.json())
+.then(user => console.log(user));
